71521e02730986cea8551c95d16de4cac3d004c1
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-12-10 20:52:06 +0530

    feat: add lots of new stuff

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-    images: {
-      domains: ["res.cloudinary.com"],
-    },
-  };
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress color="#000" options={{ showSpinnner: false }} />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
